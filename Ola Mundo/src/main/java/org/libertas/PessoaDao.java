fbde8c7517d09493869c86915280c07501354137
Lucas Rafael Avelino <125045079+LucaoRAs@users.noreply.github.com>
2024-11-06 20:52:01 -0300

    as,dç

--- Ola Mundo/src/main/java/org/libertas/PessoaDao.java
+++ Ola Mundo/src/main/java/org/libertas/PessoaDao.java
@@ -90,7 +90,7 @@ public class PessoaDao {
 		RetornoDao retornoDao = new RetornoDao();
 		
 		try {
-			String sql = "UPDATE pessoa SET nome = ?, telefone = ?, email = ? , cidade = ?,endereco = ? ,cep = ? WHERE = idpessoa" ;
+			String sql = "UPDATE pessoa SET nome = ?, telefone = ?, email = ? , cidade = ?,endereco = ? ,cep = ?, WHERE =idpessoa " ;
 			PreparedStatement prep = con.getConnection().prepareStatement(sql);
 			prep.setString(1, p.getNome());
 			prep.setString(2, p.getTelefone());
